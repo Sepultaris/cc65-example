8839c286e47f521aa01afbf08a0f95c9afa5b16d
Sepultaris <49662778+Sepultaris@users.noreply.github.com>
2024-05-06 19:32:26 -0400

    Working example

--- compile.bat
+++ compile.bat
@@ -1,8 +1,9 @@
 rem Build with CC65 v2.19 otherwise replace crt0.s and nes.lib
 
 cd src
-..\bin\cc65.exe -v cc65-example.c
+..\bin\cc65.exe -v -DNES -DFASTCALL cc65-example.c
 ..\bin\ca65.exe -v cc65-example.s
 ..\bin\ca65.exe -v crt0.s
-..\bin\ld65.exe -v --config nes.cfg crt0.o ..\lib\nes.lib cc65-example.o -o cc65-example.nes
+..\bin\ld65.exe -v --config nes.cfg crt0.o  cc65-example.o ..\lib\nes.lib -o cc65-example.nes
+d:\nes\fceux.exe cc65-example.nes
 cd ..
